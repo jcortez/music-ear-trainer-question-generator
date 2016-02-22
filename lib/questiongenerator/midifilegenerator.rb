9a2884afc7a235a22735d8e71c4bdb75874909ca
Juan Cortez <jmcortez1213@gmail.com>
2016-02-22 13:17:14 -0800

    Implemented transposing chord notes and generating inversions (tested)

--- lib/questiongenerator/midifilegenerator.rb
+++ lib/questiongenerator/midifilegenerator.rb
@@ -20,6 +20,8 @@ module QuestionGenerator
     NOTE_HALF_STEPS_ABOVE_C = { c: 0, c_sharp_d_flat: 1, d: 2, d_sharp_e_flat: 3,
       e: 4, f: 5, f_sharp_g_flat: 6, g: 7, g_sharp_a_flat: 8, a: 9,
       a_sharp_b_flat: 10, b_c_flat: 11 }
+    # Stores the number of times you have to rotate a chord to get an inversion.
+    INVERSION_ROTATIONS = { root_pos: 0, first_inv: 1, second_inv: 2, third_inv: 3 }
     private_constant :MIDI_FILE_BPM
     private_constant :MIDI_FILE_VOLUME
     private_constant :MIDI_FILE_VELOCITY
@@ -27,8 +29,9 @@ module QuestionGenerator
     private_constant :CHORD_QUALITIES
     private_constant :CHORD_INVERSIONS
     private_constant :NOTE_HALF_STEPS_ABOVE_C
+    private_constant :INVERSION_ROTATIONS
     #TODO: add constants for:
-    # - MIDI sequeces for all C chords (major,minor, etc.) for each chord inversion
+    # - MIDI notes for all C root position chords (major,minor, etc.)
 
     def initialize(dir_to_generate_files = '.')
       @dir_to_generate_midi_files = dir_to_generate_files
@@ -40,12 +43,14 @@ module QuestionGenerator
       midi_file_id = 0
       for root in CHORD_ROOTS
         for quality in CHORD_QUALITIES
-          for inversion in CHORD_INVERSIONS
-            #TODO: finish implementing
-            #sequence = get notes for quality, inversion (will be C root) and generate sequence from it using offset NOTE_HALF_STEPS_ABOVE_C[root]
-            #write sequence to midi file
-            #write midi_file_id, root, quality, inversion to xml file as question (call QuestionXMLData object to do this)
-            #midi_file_id++
+          for inversion in CHORD_INVERSIONS #TODO: only do third inversion chords for seventh chords.
+            chord_notes = [ 60, 64, 67 ] #TODO: get chord notes for c root chord quality
+            chord_notes = transpose_chord_notes(chord_notes, root)
+            chord_notes = generate_chord_inversion(chord_notes, inversion)
+            midi_sequence = generate_chord_sequence(midi_file_id, chord_notes)
+            write_sequence_to_file(midi_file_id, midi_sequence)
+            puts "#{midi_file_id}.mid: #{root} #{quality} #{inversion}" #TODO: write data to XML using QuestionXMLData object
+            midi_file_id += 1
           end
         end
       end
@@ -53,10 +58,9 @@ module QuestionGenerator
     end
 
   private
-    # Generates a MIDI::Sequence from the given array of MIDI notes transposed up
-    # the specified amount of half steps. The Sequence will play all notes
-    # simultaniously (as a chord).
-    def generate_sequence(midi_file_id, notes, half_steps_up = 0)
+    # Generates a MIDI::Sequence from the given array of MIDI notes. The Sequence
+    # will play all notes simultaneously (as a chord).
+    def generate_chord_sequence(midi_file_id, chord_notes)
       sequence = MIDI::Sequence.new
       track = MIDI::Track.new(sequence)
       track.events << MIDI::Tempo.new(MIDI::Tempo.bpm_to_mpq(MIDI_FILE_BPM))
@@ -70,8 +74,8 @@ module QuestionGenerator
 
       note_length = sequence.note_to_delta('whole')
       track.events << MIDI::ProgramChange.new(0,1,0)
-      notes.each { |note| track.events << MIDI::NoteOn.new(0, note + half_steps_up, MIDI_FILE_VELOCITY, 0) }
-      notes.each { |note| track.events << MIDI::NoteOff.new(0, note + half_steps_up, MIDI_FILE_VELOCITY, note_length) }
+      chord_notes.each { |note| track.events << MIDI::NoteOn.new(0, note, MIDI_FILE_VELOCITY, 0) }
+      chord_notes.each { |note| track.events << MIDI::NoteOff.new(0, note, MIDI_FILE_VELOCITY, note_length) }
 
       sequence.tracks << track
       sequence
@@ -80,6 +84,28 @@ module QuestionGenerator
     # Writes a MIDI::Sequence to a MIDI file.
     def write_sequence_to_file(midi_file_id, sequence)
       File.open("#{@dir_to_generate_midi_files}/#{midi_file_id}.mid", 'wb') { |file| sequence.write(file) }
+    end
+
+    # Transposes the chord notes to be a new chord at the new root specified. A
+    # new array is returned as a result.
+    def transpose_chord_notes(chord_notes, new_root)
+      chord_notes.map { |note| note + NOTE_HALF_STEPS_ABOVE_C[new_root] }
+    end
+
+    # Rotates the notes of the root position chord specified the appropriate
+    # amount of times to generate the chord inversion. This is done by
+    # transposing the appropriate notes up an octave. A new array is returned as
+    # a result.
+    def generate_chord_inversion(chord_notes, inversion)
+      chord_notes_inversion = Array.new(chord_notes)
+      num_of_rotations = INVERSION_ROTATIONS[inversion]
+      return chord_notes_inversion if num_of_rotations <= 0
+
+      for i in 0..num_of_rotations-1
+        chord_notes_inversion[i] += 12
+      end
+
+      chord_notes_inversion
     end
   end
 end
